74ddd914cf3b45818210a813771bfadf29d47b71
Hassan Usaid Ahmad <ahmad12p@uwindsor.ca>
2022-01-14 22:55:12 -0500

    added compoenents folder & added Navbar compoenent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-    return (
-        <div className="flex justify-between p-6 bg-violet-500">
-            <div>LOGO</div>
-            <div className="flex p-6">
-                <p className="m-2">Home</p>
-                <p className="m-2">About</p>
-                <p className="m-2">Courses</p>
-                <p className="m-2">Contact Us</p>
-            </div>
-        </div>
-    );
+    return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+function Navbar() {
+    return (
+        <div className="flex justify-between p-6 bg-violet-500">
+            <div>LOGO</div>
+            <div className="flex p-6">
+                <p className="m-2">Home</p>
+                <p className="m-2">About</p>
+                <p className="m-2">Courses</p>
+                <p className="m-2">Contact Us</p>
+            </div>
+            {/* 
+            Double Navbar
+            Section 1: Art w/ headline
+            Section 2: why us
+            Section 3: testimonails
+            Section 4: about the CEO/Owner/Founder
+            Section 5: sign up and shit
+            Section 6:
+            Section 7: join discrod pug
+            Footer: Plug everyhting
+            */}
+        </div>
+    );
+}
+
+export default Navbar;
