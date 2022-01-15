7db0e2e91ce7f9f2f263cfc8541e94406e9ddc29
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-01-14 21:44:02 -0500

    Initial kermit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className="flex justify-between p-6 bg-violet-500">
+            <div>LOGO</div>
+            <div className="flex p-6">
+                <p className="m-2">Home</p>
+                <p className="m-2">About</p>
+                <p className="m-2">Courses</p>
+                <p className="m-2">Contact Us</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
