d589b14299267387a232d39f329d19c8e6be8a1c
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-02-03 20:03:34 -0500

    Adding Tailwind CSS back into project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"primary-color-400": "#1b1464",
+				"primary-color-100": "#48378E",
+				"primary-color-10": "#9E9DAD",
+				"secondary-color-400": "#9f005d",
+				"secondary-color-100": "#D5488B",
+				"secondary-color-10": "#F7F2F4",
+			},
+		},
+	},
+	plugins: [],
+};
