bb2d8f6839ed3b0d33b2d0d86edd4140ca489f8e
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-02-08 21:17:38 -0500

    #24 Removed unwanted icon import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faYoutube,
 	faTwitter,
-	faInstagramSquare,
 	faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 
