7dddd8068b44e3cb9d94a81f858d656337d3b6b8
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-02-12 16:00:55 -0500

    #15 WhyWCC padding fixed for desktop/mobile

--- src/components/HomePage/WhyWCC.js
+++ src/components/HomePage/WhyWCC.js
@@ -6,30 +6,29 @@ function WhyWCC() {
 	return (
 		<div className="bg-onyx-indigo-color text-white px-14 py-32">
 			<h1 className="h1-text text-center">WHY WINDSOR CODE CAMP?</h1>
-			<p className="body-text text-center">
+			<p className="body-text text-center m-auto max-w-xl">
 				We teach from experience. Our educators are top-tier programmers
-				who <br /> will show you the skills you need.
+				who will show you the skills you need.
 			</p>
 			<div>
 				{/* Point 1 */}
-				<div className="flex xs:flex-col md:flex-row justify-center items-center xs:py-16 md:py-32">
+				<div className="flex xs:flex-col md:flex-row justify-center items-center xs:py-16 md:py-16 md:pt-32">
 					<div className="xs:pr-0 md:pr-48 xs:order-2 md:order-1 xs:text-center md:text-left">
 						<p className="h3-text text-peach-tone-color ">
 							LEARN AT YOUR OWN PACE
 						</p>
-						<p className="body-text">
-							Learn on your terms, but never alone. <br /> Enjoy
-							access to your course with access <br /> to the
-							Discord Server
+						<p className="body-text max-w-sm">
+							Learn on your terms, but never alone. Enjoy access
+							to your course with access to the Discord Server
 						</p>
 					</div>
-					<div className="xs:order-1 md:order-2">
+					<div className="xs:order-1 md:order-2 md:mb-0 xs:mb-16">
 						<img src={img1} alt="" />
 					</div>
 				</div>
 				{/* Point 2 */}
-				<div className="flex xs:flex-col md:flex-row justify-center items-center xs:py-16 md:py-32 xs:text-center md:text-left">
-					<div className="xs:pr-0 md:pr-48">
+				<div className="flex xs:flex-col md:flex-row justify-center items-center xs:py-16 md:py-16 xs:text-center md:text-left">
+					<div className="xs:pr-0 md:pr-48 md:mb-0 xs:mb-16">
 						<img src={img2} alt="" />
 					</div>
 					<div>
@@ -54,7 +53,7 @@ function WhyWCC() {
 							for answers and <br /> feedback.
 						</p>
 					</div>
-					<div className="xs:order-1 md:order-2">
+					<div className="xs:order-1 md:order-2 xs:mb-16">
 						<img src={img3} alt="" />
 					</div>
 				</div>
