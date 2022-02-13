cd74116945261ee9243b2bfcf92405e7ba29db50
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-02-13 01:04:28 -0500

    #30 Made array from the points in WhyWCC section

--- src/components/HomePage/WhyWCC.js
+++ src/components/HomePage/WhyWCC.js
@@ -3,60 +3,70 @@ import img2 from "../../assets/images/HomePage/whywccImg2.png";
 import img3 from "../../assets/images/HomePage/whywccImg3.png";
 
 function WhyWCC() {
+	const points = [
+		{
+			title: "LEARN AT YOUR OWN PACE",
+			body: "Learn on your terms, but never alone. Enjoy access to your course with access to the Discord Server",
+			img: img1,
+		},
+		{
+			title: "BUILD YOUR NETWORK",
+			body: "Join a growing community, get peer insights, and discover exciting project opportunities and collaborations.",
+			img: img2,
+		},
+		{
+			title: "GET EXPERT SUPPORT",
+			body: "Our team of experts is always a message away for answers and feedback.",
+			img: img3,
+		},
+	];
+
 	return (
-		<div className="bg-onyx-indigo-color text-white px-14 py-32">
+		<div className="bg-onyx-indigo-color text-white px-14 pt-32 pb-16">
 			<h1 className="h1-text text-center">WHY WINDSOR CODE CAMP?</h1>
 			<p className="body-text text-center m-auto max-w-xl">
 				We teach from experience. Our educators are top-tier programmers
 				who will show you the skills you need.
 			</p>
 			<div>
-				{/* Point 1 */}
-				<div className="flex xs:flex-col md:flex-row justify-center items-center xs:py-16 md:py-16 md:pt-32">
-					<div className="xs:pr-0 md:pr-48 xs:order-2 md:order-1 xs:text-center md:text-left">
-						<p className="h3-text text-peach-tone-color ">
-							LEARN AT YOUR OWN PACE
-						</p>
-						<p className="body-text max-w-sm">
-							Learn on your terms, but never alone. Enjoy access
-							to your course with access to the Discord Server
-						</p>
-					</div>
-					<div className="xs:order-1 md:order-2 md:mb-0 xs:mb-16">
-						<img src={img1} alt="" />
-					</div>
-				</div>
-				{/* Point 2 */}
-				<div className="flex xs:flex-col md:flex-row justify-center items-center xs:py-16 md:py-16 xs:text-center md:text-left">
-					<div className="xs:pr-0 md:pr-48 md:mb-0 xs:mb-16">
-						<img src={img2} alt="" />
-					</div>
-					<div>
-						<p className="h3-text text-peach-tone-color ">
-							BUILD YOUR NETWORK
-						</p>
-						<p className="body-text max-w-sm">
-							Join a growing community, get peer insights, and
-							discover exciting project opportunities and
-							collaborations.
-						</p>
-					</div>
-				</div>
-				{/* Point 3 */}
-				<div className="flex xs:flex-col md:flex-row justify-center items-center xs:pt-16 md:pt-32 xs:text-center md:text-left">
-					<div className="xs:pr-0 md:pr-48 xs:order-2 md:order-1">
-						<p className="h3-text text-peach-tone-color ">
-							GET EXPERT SUPPORT
-						</p>
-						<p className="body-text max-w-sm">
-							Our team of experts is always a message away for
-							answers and feedback.
-						</p>
-					</div>
-					<div className="xs:order-1 md:order-2 xs:mb-16">
-						<img src={img3} alt="" />
-					</div>
-				</div>
+				{points.map((point, i) => {
+					return (
+						<div
+							key={i}
+							className={
+								`flex xs:flex-col md:flex-row
+								justify-center items-center xs:py-16 md:py-16
+								` + (i == 0 && "md:pt-32 xs:pt-32")
+							}
+						>
+							<div
+								className={
+									`xs:text-center md:text-left ` +
+									(i % 2 == 0
+										? "xs:order-2 md:order-1 xs:pr-0 md:pr-48"
+										: "xs:order-2 md:order-2")
+								}
+							>
+								<p className="h3-text text-peach-tone-color">
+									{point.title}
+								</p>
+								<p className="body-text max-w-sm">
+									{point.body}
+								</p>
+							</div>
+							<div
+								className={
+									"md:mb-0 xs:mb-16 " +
+									(i % 2 == 0
+										? "xs:order-1 md:order-2"
+										: "xs:order-1 md:order-1 xs:pr-0 md:pr-48")
+								}
+							>
+								<img src={point.img} alt="" />
+							</div>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
