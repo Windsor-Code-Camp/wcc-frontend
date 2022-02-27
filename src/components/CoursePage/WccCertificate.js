17b4953f4ba8faa760ad69b63ab958d494ca7d7a
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-02-27 15:16:40 -0500

    #63 Changed certificate padding top to 64px

--- src/components/CoursePage/WccCertificate.js
+++ src/components/CoursePage/WccCertificate.js
@@ -4,7 +4,7 @@ const WccCertificate = ({ certificateURL }) => {
 	return (
 		<PageSection className="md:py-0 xs:py-0 md:pb-32 xs:pb-16">
 			<h1 className="h1-text text-center">THE WCC CERTIFICATE</h1>
-			<div className="pt-32 flex justify-center">
+			<div className="pt-16 flex justify-center">
 				<img src={certificateURL} className="shadow-lg" alt="" />
 			</div>
 		</PageSection>
