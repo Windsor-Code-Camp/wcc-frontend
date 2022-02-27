6f5b5dc1472f2ddeff27724c9ced23fb4cf94442
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-02-27 16:54:07 -0500

    #62 Video padding top is 64px now

--- src/components/AboutUsPage/FromOurCEO.js
+++ src/components/AboutUsPage/FromOurCEO.js
@@ -1,16 +1,17 @@
 import ReactPlayer from "react-player";
+import PageSection from "../PageSection";
 
 const FromOurCEO = () => {
 	return (
-		<div className="px-14 text-center">
+		<PageSection className="xs:py-0 md:py-0 px-14 text-center">
 			<h1 className="h1-text">FROM OUR CEO</h1>
-			<div className="flex justify-center xs:py-20 md:py-32">
+			<div className="flex justify-center xs:py-20 md:pt-16">
 				<ReactPlayer
 					controls
 					url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 				/>
 			</div>
-		</div>
+		</PageSection>
 	);
 };
 
